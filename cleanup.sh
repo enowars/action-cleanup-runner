82c822d99b9dc6cbd446fbecfbc313f9999b2601
Christoph Witzko <github@christophwitzko.com>
2021-04-28 19:28:17 +0200

    feat: cleanup working directory

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
-echo "running cleanup...."
-
+echo "removing all docker containers..."
 docker ps -aq | xargs docker rm -f
+
+echo "cleaning working directory ($(pwd))..."
+rm -rf *
