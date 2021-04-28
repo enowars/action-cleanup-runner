25203d842dfa7ab0d31ad475e5c6a0dea12dc950
Christoph Witzko <github@christophwitzko.com>
2021-04-28 20:53:15 +0200

    feat: run docker system prune

--- cleanup.sh
+++ cleanup.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 echo "removing all docker containers..."
 docker ps -aq | xargs docker rm -f
 
+echo "removing docker networks, volumes and images..."
+docker system prune -af --volumes
+
 echo "cleaning up working directory..."
 shopt -s dotglob
 rm -rf *
