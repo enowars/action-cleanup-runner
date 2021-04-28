3669ffc0856dc53e332207b0b8f29a2b54b00ee4
Christoph Witzko <github@christophwitzko.com>
2021-04-28 21:02:50 +0200

    fix: allow failures

--- cleanup.sh
+++ cleanup.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 echo "removing all docker containers..."
-docker ps -aq | xargs docker rm -f
+docker ps -aq | xargs docker rm -f || true
 
 echo "removing docker networks, volumes and images..."
-docker system prune -af --volumes
+docker system prune -af --volumes || true
 
 echo "cleaning up working directory..."
 shopt -s dotglob
